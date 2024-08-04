d9281d11443118bfeafe2e536464492651ea4110
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-08-05 03:08:21 +0700

    refactor: rename file css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.css"
+import "./global.css"
 import { Root } from "./route"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
