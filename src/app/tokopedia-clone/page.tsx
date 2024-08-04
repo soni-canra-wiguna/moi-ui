446471bdf01be939e8fbdbe14a2d0abcc2c38584
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-08-05 03:55:50 +0700

    chore: setup color tokopedia clone

--- src/app/tokopedia-clone/page.tsx
+++ src/app/tokopedia-clone/page.tsx
@@ -1,12 +1,9 @@
-import React from "react"
-import { useLocation } from "react-router-dom"
 import useDocumentTitle from "../../hook/use-document-title"
 
 const TokoPediaClonePage = () => {
-  const { pathname } = useLocation()
-  useDocumentTitle(pathname)
+  useDocumentTitle("situs jual beli online terlengkap")
   // add clasName: font, bg color, text folor, dan lain lain
-  return <div></div>
+  return <div className="font-openSauceRegular"></div>
 }
 
 export default TokoPediaClonePage
