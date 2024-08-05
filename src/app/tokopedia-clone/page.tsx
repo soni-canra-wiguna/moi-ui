db4ec2990fca104408ee2416b75e0011cc82a35d
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-08-05 20:24:52 +0700

    feat(tokped-clone): add header

--- src/app/tokopedia-clone/page.tsx
+++ src/app/tokopedia-clone/page.tsx
@@ -1,9 +1,14 @@
 import useDocumentTitle from "../../hook/use-document-title"
+import Header from "./_components/Header"
 
 const TokoPediaClonePage = () => {
   useDocumentTitle("situs jual beli online terlengkap")
   // add clasName: font, bg color, text folor, dan lain lain
-  return <div className="font-openSauceRegular"></div>
+  return (
+    <div className="w-full min-h-screen font-openSauceRegular bg-tpbackground text-tptext">
+      <Header />
+    </div>
+  )
 }
 
 export default TokoPediaClonePage
